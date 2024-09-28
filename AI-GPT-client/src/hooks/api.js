11359b5d656cb909724b552fbad5e60b53cf50fe
Arush Tripathi <139771140+arushtripathi121@users.noreply.github.com>
2024-09-28 19:05:21 +0530

    Update api.js

--- AI-GPT-client/src/hooks/api.js
+++ AI-GPT-client/src/hooks/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/gpt/'
+    baseURL: 'https://ai-gpt-kohl.vercel.app/api/v1/gpt/'
 })
 
 export const googleAuth = (code) => api.get(`/google?code=${code}`)
