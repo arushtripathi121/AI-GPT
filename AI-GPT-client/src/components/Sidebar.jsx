d282e1b20381150659288e12cb116b579430e428
arushtripathi121 <arushtripathi121@gamail.com>
2024-09-28 15:00:20 +0530

    c

--- AI-GPT-client/src/components/Sidebar.jsx
+++ AI-GPT-client/src/components/Sidebar.jsx
@@ -1,15 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import closeButton from '../assets/close_button.png'
-// import dotenv from 'dotenv';
-// dotenv.config();
 
-const Sidebar = ({closeMenu}) => {
+const Sidebar = ({ closeMenu, email, conversationId}) => {
+
+    const [conversationData, setConversationData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchConversation = async (email) => {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:3000/api/v1/gpt/getConversationIds', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email })
+        });
+        const data = await response.json();
+        setConversationData(data.data);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        fetchConversation(email);
+    }, [])
+
     return (
-        <div className='w-64 h-full border-r border-white text-white pr-5 pb-5'>
-            <div className='flex flex-row items-center justify-between'>
-                <p>Recent Conversations</p>
-                <img src={closeButton} onClick={closeMenu} className='bg-white rounded-xl w-8 h-8 border border-black cursor-pointer'/>
+        <div className='w-64 h-full bg-black border-r border-gray-800 text-white pr-5 pb-5'>
+            <div className='flex items-center justify-between p-4 border-b border-gray-800'>
+                <h2 className='text-lg font-bold'>Recent Conversations</h2>
+                <img
+                    src={closeButton}
+                    onClick={closeMenu}
+                    className='w-8 h-8 bg-white rounded-full hover:bg-gray-400 transition duration-200 cursor-pointer'
+                    alt="Close menu"
+                />
             </div>
+
+            {isLoading ? (
+                <div className='flex items-center justify-center'>
+                    <p>Loading Conversations...</p>
+                </div>
+            ) : (
+                <div className='pt-4 overflow-y-auto h-[calc(100%-4rem)]'>
+                    {conversationData.length > 0 ? (
+                        conversationData.map((m, index) => (
+                            <div
+                                key={index}
+                                className='p-2 border-b border-white hover:bg-gray-800 rounded transition duration-150 cursor-pointer'
+                                onClick={() => {conversationId(m.conversationId)}}
+                            >
+                                {m.firstChat}
+                            </div>
+                        ))
+                    ) : (
+                        <div className='flex items-center justify-center h-full'>
+                            <p>No conversations available.</p>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     )
 }
--- AI-GPT-client/src/pages/HomePage.jsx
+++ AI-GPT-client/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import menuButton from '../assets/menu-button.png';
 import Sidebar from '../components/Sidebar';
@@ -6,32 +6,115 @@ import Sidebar from '../components/Sidebar';
 const HomePage = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const data = localStorage.getItem('user-info');
+  const userData = JSON.parse(data);
+  const email = userData?.email;
+  const [currentConversationId, setCurrentConversationId] = useState('');
+  const [conversationData, setConversationData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [prompt, setPrompt] = useState('');
 
   const handleMenuButton = () => {
     setIsMenuOpen(!isMenuOpen);
   }
-  return ( 
+
+  const fetchDataById = async (currentConversationId) => {
+    if (currentConversationId != null) {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:3000/api/v1/gpt/getChats', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ conversationId: currentConversationId })
+      });
+      const data = await response.json();
+      setConversationData(data.data);
+      console.log(conversationData);
+      setIsLoading(false);
+    }
+  }
+
+  const askNewQuestion = async () => {
+    setIsLoading(true);
+    const response = await fetch('http://localhost:3000/api/v1/gpt/prompt', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt, email })
+    });
+    const data = await response.json();
+    setConversationData(data.data);
+    console.log(conversationData);
+    setIsLoading(false);
+  }
+
+  const handleAskButton = () => {
+    
+  }
+
+  useEffect(() => {
+    fetchDataById(currentConversationId);
+  }, [currentConversationId])
+
+
+  return (
     <section className='w-screen h-screen bg-black flex flex-col'>
       <div className='flex-none'>
         <Header home={true} />
       </div>
 
-      <div className='flex flex-row items-start pt-5 px-8 flex-grow h-full w-full'>
-        
-        <div className=' h-full pb-5'>{isMenuOpen ? <Sidebar closeMenu={handleMenuButton}/> : <img src={menuButton} onClick={handleMenuButton} className='w-8 h-8 cursor-pointer' alt="Menu Button" />}</div>
-        
-        <div className='flex-grow p-5 pt-0 flex flex-col h-full w-full'>
-          <div className='bg-black rounded-lg flex-grow p-5 overflow-y-auto space-y-4 h-full'>
-            <div className='text-white'></div>
+      <div className='flex flex-row items-start pt-5 pl-8 flex-grow h-full w-full'>
+
+        <div className=' h-full pb-5'>{isMenuOpen ? <Sidebar email={email} conversationId={(id) => { setCurrentConversationId(id) }} closeMenu={handleMenuButton} /> : <img src={menuButton} onClick={handleMenuButton} className='w-8 h-8 cursor-pointer' alt="Menu Button" />}</div>
+
+        <div className='flex-grow pl-5 pt-0 flex flex-col h-full w-full'>
+          <div className='bg-black rounded-lg flex-grow overflow-y-auto space-y-4 h-[70svh] p-4'>
+            <div className='text-white'>
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                Array.isArray(conversationData) && conversationData.length > 0 ? (
+                  conversationData.map((m, index) => (
+                    <div key={index} className='border-b border-gray-700 pb-4'>
+                      <p className='font-semibold text-blue-400'>Prompt:</p>
+                      <p className='mb-2 italic'>{m.prompt}</p>
+                      <p className='font-semibold text-blue-400'>Response:</p>
+                      <div
+                        className='rounded-lg p-2'
+                        dangerouslySetInnerHTML={{
+                          __html: m.response
+                            .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+                            .replace(/### (.*?)\n/g, '<h3>$1</h3>')
+                            .replace(/## (.*?)\n/g, '<h2>$1</h2>')
+                            .replace(/\* (.*?)\n/g, '<li>$1</li>')
+                            .replace(/\n/g, '<br />')
+                        }}
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <div className="flex items-center justify-center h-full bg-black">
+                    <p className="text-white text-lg">Start a new conversation</p>
+                  </div>
+                )
+              )}
+            </div>
           </div>
 
-          <div className='border border-white bg-black rounded-lg mt-5 flex flex-row items-center'>
-            <input
-              type='text'
-              className='w-full bg-transparent text-white border-none focus:outline-none placeholder-gray-400 p-3'
-              placeholder='Enter your prompt'
-            />
-            <button className='bg-white p-3 text-black font-medium text-xl px-10 rounded-r-lg'>Ask</button>
+
+          <div className=' bg-black  pt-10 px-5 pb-5'>
+            <div className='border border-white w-full flex flex-row items-center rounded-lg'>
+              <input
+                type='text'
+                value={prompt}
+                className='w-full bg-transparent text-white border-none focus:outline-none placeholder-gray-400 p-3'
+                placeholder='Enter your prompt'
+                onChange={e => setPrompt(e.target.val)}
+              />
+              <button className='bg-white p-3 text-black font-medium text-xl px-10 rounded-r-lg'>Ask</button>
+            </div>
           </div>
         </div>
       </div>
